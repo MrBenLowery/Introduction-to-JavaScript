ef1ecb2037aec9c8853f297f3c23c43dc43f0d88
Bennett-Lowery <mrbennettlowery@gmail.com>
2021-12-07 21:31:01 -0600

    ACTUALLY finished the tasks after painstakingly plugging away at the dog question

--- index.js
+++ index.js
@@ -117,27 +117,44 @@ Puppies less than 1 year
   
 NOTE: If done correctly, a weight of 15 lbs and age of 1 year would return 0.44999999999999996
 */
-
 function hungryDog(weight, age) {
   if (age >= 1 && weight <= 5) {
-    return weight * 0.05;
-  } else if (age >= 1 && weight > 6 && weight <= 10) {
-    return weight * 0.04;
-  } else if (age >= 1 && weight >= 11 && weight <= 15) {
-    return weight * 0.03;
-  } else if (age >= 1 && weight > 15) {
-    return weight * 0.02;
+    return weight * .05;
+  } else if (age >= 1 && weight <= 10) {
+    return weight * .04;
+  } else if (age >= 1 && weight <= 15) {
+    return weight * .03;
+  } else if (age >= 1 && weight <= 151) {
+    return weight * .02;
+  } else if (age < .1667) {
+    return "Should be fed via nursing from mother";
+  } else if (age < .3333) {
+    return weight * .1;
+  } else if (age < .583) {
+    return weight * .05;
   } else if (age < 1) {
-    if (age > (2 / 12) && age < (4 / 12)) {
-      return weight * 0.10;
-    } else if (age > (4 / 12) && age < (7 / 12)) {
-      return weight * 0.05;
-    } else if (age > (7 / 12) && age < (12 / 12)) {
-      return weight * 0.04;
-    }
+    return weight * .04;
   }
 }
 
+// function hungryDog(weight, age) {
+//   if (age >= 1 && weight <= 5) {
+//     return weight * 0.05;
+//   } else if (age >= 1 && weight > 6 && weight <= 10) {
+//     return weight * 0.04;
+//   } else if (age >= 1 && weight >= 11 && weight <= 15) {
+//     return weight * 0.03;
+//   } else if (age >= 1 && weight > 15) {
+//     return weight * 0.02;
+//   } else if (age > 0.33 && age < 0.5833) {
+//     return weight * 0.10;
+//   } else if (age > 0.5833 && age < 1) {
+//     return weight * 0.10;
+//   } else {
+//     return weight * 0.04;
+//   }
+// }
+// hungryDog(4, 0.583);
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
@@ -171,7 +188,7 @@ if (computer === 0) {
 }
 function game(user, computer) {
   if (user === computer) {
-    return "It's a tie!"
+    return "it's a tie"
   } else if (user === 'rock' && computer === 'paper') {
     return "you lose!"
   } else if (user === 'paper' && computer === 'scissors') {
@@ -231,7 +248,7 @@ Using the annoyingSong function below do the following:
 
 function annoyingSong(number) {
   for (let i = number; i >= 0; i--) {
-    return `${i} bottles of soda on the wall, ${i} bottles of soda, take one down, pass it around ${i - 1} bottles of soda on the wall`;
+    return `${i} bottles of soda on the wall, ${i} bottles of soda, take one down pass it around ${i - 1} bottles of soda on the wall`;
   }
 }
 console.log(annoyingSong(20));
